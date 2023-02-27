9b43294cd0bf48077b4151dceea6416aa5ac99b0
zyonson <wakeup5216@gmail.com>
2023-02-27 19:23:06 +0900

    add model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,21 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
+    it "is invalid if email is invalid format" do
+      user = User.new(email: 'aaa')
+      user.valid?
+      expect(user.errors[:email]).to include('is invalid')
+    end
+    it 'is invalid if email is more than 255 characters ' do
+      user = User.new(email: 'a' * 256)
+      user.valid?
+      expect(user.errors[:email]).to include('is too long (maximum is 255 characters)')
+    end
+
+    it 'is invalid if userpassword is less than 6 characters' do
+      user = User.new(password: 'a' * 5)
+      user.valid?
+      expect(user.errors[:password]).to include('is too short (minimum is 6 characters)')
+    end
   end
 end
