f475a2d67dfc88bc0114f1ecf77687af5a1f8614
zyonson <wakeup5216@gmail.com>
2023-03-09 01:38:30 +0900

    session replay

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,7 +11,8 @@ module SessionsHelper
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      @current_user = user if user && (session[:session_token] = user.session_token)
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
   end
 
   def authenticated?(remember_token)
@@ -37,5 +38,9 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def session_token
+    remember_digest || remember
   end
 end
