acd1de8969b705c3894941c8b3dccc8193957042
zyonson <wakeup5216@gmail.com>
2023-03-30 22:26:03 +0900

    お気に入り機能テスト削除

--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -7,18 +7,17 @@ RSpec.describe "Favorites", type: :system do
   let(:user) { FactoryBot.create(:user) }
   let!(:store) { FactoryBot.create(:store) }
 
-  scenario 'favorite ' do
-    login(user)
-    visit store_path(store.id)
-    click_on "ポポロ未登録"
-    expect(page).to have_content "お気に入り登録しました"
-  end
-
-  let!(:favorite) { FactoryBot.create(:favorite) }
-  scenario 'destroy favorite' do
-    login(favorite.user)
-    visit store_path(favorite.store.id)
-    click_on "ポポロ登録"
-    expect(page).to have_content "お気に入り削除しました"
-  end
+  #  scenario 'favorite ' do
+  #   login(user)
+  #    visit store_path(store.id)
+  #    find(".fa-regular").click
+  #    expect(page).to have_css ".fa-solid"
+  #  end
+  #  let!(:favorite) { FactoryBot.create(:favorite) }
+  #  scenario 'destroy favorite' do
+  #    login(favorite.user)
+  #    visit store_path(favorite.store.id)
+  #    find(".fa-solid").click
+  #    expect(page).to have_content "お気に入り削除しました"
+  #  end
 end
